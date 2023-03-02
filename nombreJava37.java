2d6e292d8c609a1cbb38548b1028bddf5585a6c7
david37 <david37@examen.com>
2023-03-02 09:56:09 +0000

    v1

--- nombreJava37.java
+++ nombreJava37.java
@@ -1,1 +1,5 @@
-System.out.println("Me llamo David y he creado el servidor ubunserver37");
+public class nombreJava37 {
+	public static void main(String[] args){
+		System.out.println("Me llamo David y he creado el servidor ubunserver37");
+	}
+}	
